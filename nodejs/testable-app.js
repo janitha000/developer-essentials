79f45f09ddf28cbedfa2f16f7773a00089c92137
Janitha Tennakoon <janitha000@gmail.com>
2021-08-20 09:03:02 +0530

    Add nodejs examples

---
+++ nodejs/testable-app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const logger = require('./utils/logger');
+const leaseRouter = require('./routes/lease-routes')
+const handleErrors = require('./middlewares/errorHandleMiddleware')
+const protectedRoute = require('./middlewares/authMiddleware')
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use('/leases', protectedRoute, leaseRouter)
+app.use(handleErrors);
+
+// const swaggerDocument = YAML.load('./swagger.yaml');
+const swaggerDocument = require('./swagger.json');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+
+
+const server = app.listen(PORT, () => {
+    logger.info("🚀 The server started on port " + PORT)
+    console.log("🚀 The server started on port " + PORT)
+});
+
+module.exports = { app, server };
+
+
+
+// const { app } = require('../server');
+// const request = require('supertest');
+// const res = await request(app)
+//             .get('/leases/ledger')
+//             .set('Authorization', 'Bearer ' + token)
+//             .query(queryParams)
+// expect(res.status).toBe(200);
