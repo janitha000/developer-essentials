944b3f6e842aa2ee652ed274b468de3670bce53c
Janitha Tennakoon <janitha000@gmail.com>
2021-09-24 16:38:11 +0530

    add configuration variables

---
+++ dotnetcore/Configurations.cs
@@ -0,0 +1,29 @@
+appsettings.json
+{
+  "Logging": {
+    "LogLevel": {
+      "Default": "Information",
+      "Microsoft": "Warning",
+      "Microsoft.Hosting.Lifetime": "Information"
+    }
+  },
+  "BaseUrl": {
+    "Type": "https",
+    "Url": "localhost:5001",
+    "Path": "api"
+  }
+}
+
+public class BaseConfiguration
+{
+    public string Type { get; set; }
+    public string Url { get; set; }
+    public string Path { get; set; }
+}
+
+public WeatherForecastController( IOptions<BaseConfiguration> options)
+        {
+            Options = options;
+        }
+
+var settings = Options.Value;
