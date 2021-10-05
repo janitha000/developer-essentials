064907aa39e4def7dd12201802c47addf42b35ba
Janitha Tennakoon <janitha000@gmail.com>
2021-10-05 16:37:56 +0530

    add ng-content example

---
+++ angular/ng-content.cs
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-zippy-basic',
+  template: `
+    <h2>Single-slot content projection</h2>
+    <ng-content></ng-content>
+  `
+})
+export class ZippyBasicComponent {}
+
+<app-zippy-basic>
+  <p>Is content projection cool?</p>
+</app-zippy-basic>
+
+//multi-slot
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-zippy-multislot',
+  template: `
+    <h2>Multi-slot content projection</h2>
+
+    Default:
+    <ng-content></ng-content>
+
+    Question:
+    <ng-content select="[question]"></ng-content>
+  `
+})
+export class ZippyMultislotComponent {}
+
+<app-zippy-multislot>
+  <p question>
+    Is content projection cool?
+  </p>
+  <p>Let's learn about content projection!</p>
+</app-zippy-multislot>
